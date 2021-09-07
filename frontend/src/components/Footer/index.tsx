ca473d2d06941c51e14f4f709b7e2b0065146540
Carlos Rodrigues <carloseduardors088@gmail.com>
2021-09-06 23:12:36 -0300

    Componentes basicos estaticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+
+function footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/cadu088" target="_blank" rel="noreferrer">Carlos Rodrigues</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default footer;
+
+
